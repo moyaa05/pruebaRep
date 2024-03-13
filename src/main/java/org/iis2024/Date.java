580ba0397b516baaf7845efef6ec916f51c0ec6b
moyaa05 <antomm05@gmail.com>
2024-03-13 14:21:36 +0100

    Cambio del autor en el constructor

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -53,18 +53,11 @@ public class Date {
     return year;
   }
 
-  /**
-   * Constructs a Date object representing a specific day, month, and year.
-   *
-   * @param day The day of the month (1-31).
-   * @param month The month of the year (1-12).
-   * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
-   *     to pass).
-   */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+
+  public Date(int dia, int mes, int ano){
+    this.day = dia;
+    this.month = mes;
+    this.year = ano;
   }
 
   /**
