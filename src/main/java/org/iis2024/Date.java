93ab6757d71226c43e98038003c1867072075cdc
sergiontheplanet <sergiontheplanet@gmail.com>
2024-03-13 14:07:53 +0100

    Variable created by the collaborator

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,6 +20,8 @@ public class Date {
   private final int month;
   private final int year;
 
+  private int fileAddedBySergioCollaborator;
+
   /**
    * Retrieves the day component of this date.
    *
