b963cb8d2de7f971f84e728b47e7b95a8f7bd5d2
moyaa05 <antomm05@gmail.com>
2024-03-13 14:06:48 +0100

    Method created by the author

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,6 +20,11 @@ public class Date {
   private final int month;
   private final int year;
 
+
+  public void methodAddedByAntonioAuthor(){
+    System.out.println("Method added by the owner Antonio");
+  }
+
   /**
    * Retrieves the day component of this date.
    *
